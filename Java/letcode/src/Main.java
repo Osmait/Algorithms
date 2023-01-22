2d76c3b8685b9b86d3e4f837fa75e6e1d1cb6e72
Saul Burgos <saulburgos6@gmail.com>
2023-01-21 23:26:54 -0400

    add

--- Java/letcode/src/Main.java
+++ Java/letcode/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] lists ={"eat","tea","tan","ate","nat","bat"};
+        Solution solution = new Solution();
+        List<List<String>> result = solution.groupAnagrams(lists);
+        System.out.println(result);
+
+
     }
 }
--- Java/letcode/src/Solution.java
+++ Java/letcode/src/Solution.java
@@ -1,14 +1,65 @@
-import java.util.HashSet;
+import java.util.*;
 
 public class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate(int[] numb) {
         Set<Integer> uniques = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (uniques.contains(nums[i])) {
+        for (int num : numb) {
+            if (uniques.contains(num)) {
                 return true;
             }
-            uniques.add(nums[i]);
+            uniques.add(num);
         }
         return false;
     }
+
+    public  boolean isAnagram(String s, String t){
+        if (s.length() != t.length()) return false;
+        int[] store = new int[26];
+
+        for (int i = 0 ; i < s.length(); i++){
+            store[s.charAt(i)-'a']++;
+            store[t.charAt(i)-'a']--;
+        }
+        for (int n :store) if (n != 0 )return false;
+        return true;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return new int[] {};
+    }
+
+public List<List<String>> groupAnagrams(String[] strs){
+        List<List<String>> res = new ArrayList<>();
+        if (strs.length == 0 )return res;
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs){
+            char[] hash = new char[26];
+            for (char c : s.toCharArray()){
+                hash[c - 'a']++;
+            }
+            String key = new String(hash);
+            map.computeIfAbsent(key, k-> new ArrayList<>());
+            map.get(key).add(s);
+
+        }
+        res.addAll(map.values());
+        return res;
+}
+
+
+
 }
